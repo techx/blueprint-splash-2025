e0cf0e5fef91c19c7b2bc2e947f96ea05093360b
Annie Wang <anniewang2023@gmail.com>
2024-12-28 17:45:31 -0500

    roll back temporarily again

--- src/components/sections/Home.tsx
+++ src/components/sections/Home.tsx
@@ -7,52 +7,50 @@ import { Model } from "../Final_2.jsx";
 import { Enhanced_Final } from "../Enhanced_Final.jsx";
 
 const Home: React.FC = () => {
-  const [scale, setScale] = useState(0.6);
+  // const [scale, setScale] = useState(0.6);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setScale(window.innerWidth < 640 ? 0.4 : 0.6);
-    };
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     setScale(window.innerWidth < 640 ? 0.4 : 0.6);
+  //   };
 
-    handleResize(); // Set initial scale
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  //   handleResize(); // Set initial scale
+  //   window.addEventListener("resize", handleResize);
+  //   return () => window.removeEventListener("resize", handleResize);
+  // }, []);
 
-  return (
-    <div className="flex-grow flex h-[100vh]">
-      <Canvas
-        shadows
-        camera={{ position: [10, 5, 10], fov: 25 }}
-        className="w-full h-full"
-      >
-        <ambientLight intensity={0.8} />
-        <group position={[0, -1.2, 0]}>
-          {/* <Model scale={scale} /> */}
-          <Enhanced_Final />
-        </group>
-        <OrbitControls minDistance={10} maxDistance={24} />
-      </Canvas>
-    </div>
-  );
   // return (
-  //       {/* <nav className="space-x-4">
-  //         <Link to="/about" className="text-blue-600 hover:underline">
-  //           About
-  //         </Link>
-  //         <Link to="/tracks" className="text-blue-600 hover:underline">
-  //           Tracks
-  //         </Link>
-  //         <Link to="/faq" className="text-blue-600 hover:underline">
-  //           FAQ
-  //         </Link>
-  //         <Link to="/photos" className="text-blue-600 hover:underline">
-  //           Photos
-  //         </Link>
-  //       </nav> */}
-  //     </div>
+  //   <div className="flex-grow flex h-[100vh]">
+  //     <Canvas
+  //       shadows
+  //       camera={{ position: [10, 5, 10], fov: 25 }}
+  //       className="w-full h-full"
+  //     >
+  //       <ambientLight intensity={0.8} />
+  //       <group position={[0, -1.2, 0]}>
+  //         {/* <Model scale={scale} /> */}
+  //         <Enhanced_Final />
+  //       </group>
+  //       <OrbitControls minDistance={10} maxDistance={24} />
+  //     </Canvas>
   //   </div>
   // );
+  return (
+    <nav className="space-x-4">
+      <Link to="/about" className="text-blue-600 hover:underline">
+        About
+      </Link>
+      <Link to="/tracks" className="text-blue-600 hover:underline">
+        Tracks
+      </Link>
+      <Link to="/faq" className="text-blue-600 hover:underline">
+        FAQ
+      </Link>
+      <Link to="/photos" className="text-blue-600 hover:underline">
+        Photos
+      </Link>
+    </nav>
+  );
 };
 
 export default Home;
