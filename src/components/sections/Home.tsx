430d61c84f6165f56cd10f2fb5e693556fbb7c69
Annie Wang <anniewang2023@gmail.com>
2024-12-27 19:35:31 -0500

    comment 3d out for now

--- src/components/sections/Home.tsx
+++ src/components/sections/Home.tsx
@@ -1,55 +1,53 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import { Model } from "../FINAL_ROOM.jsx";
+// import { Canvas } from "@react-three/fiber";
+// import { OrbitControls } from "@react-three/drei";
+// import { Model } from "../FINAL_ROOM.jsx";
 
 const Home: React.FC = () => {
-  const [scale, setScale] = useState(0.6);
+  // const [scale, setScale] = useState(0.6);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setScale(window.innerWidth < 640 ? 0.4 : 0.6);
-    };
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     setScale(window.innerWidth < 640 ? 0.4 : 0.6);
+  //   };
 
-    handleResize(); // Set initial scale
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
+  //   handleResize(); // Set initial scale
+  //   window.addEventListener('resize', handleResize);
+  //   return () => window.removeEventListener('resize', handleResize);
+  // }, []);
 
-  return (
-    <div className="flex-grow flex h-[100vh]">
-      <Canvas
-        shadows
-        camera={{ position: [10, 5, 10], fov: 25 }}
-        className="w-full h-full"
-      >
-        <ambientLight intensity={1} />
-        <group position={[0, -1.2, 0]}>
-          <Model scale={scale} />
-        </group>
-        <OrbitControls minDistance={10} maxDistance={24} />
-      </Canvas>
-    </div>
-  );
   // return (
-  //       {/* <nav className="space-x-4">
-  //         <Link to="/about" className="text-blue-600 hover:underline">
-  //           About
-  //         </Link>
-  //         <Link to="/tracks" className="text-blue-600 hover:underline">
-  //           Tracks
-  //         </Link>
-  //         <Link to="/faq" className="text-blue-600 hover:underline">
-  //           FAQ
-  //         </Link>
-  //         <Link to="/photos" className="text-blue-600 hover:underline">
-  //           Photos
-  //         </Link>
-  //       </nav> */}
-  //     </div>
+  //   <div className="flex-grow flex h-[100vh]">
+  //     <Canvas
+  //       shadows
+  //       camera={{ position: [10, 5, 10], fov: 25 }}
+  //       className="w-full h-full"
+  //     >
+  //       <ambientLight intensity={1} />
+  //       <group position={[0, -1.2, 0]}>
+  //         <Model scale={scale} />
+  //       </group>
+  //       <OrbitControls minDistance={10} maxDistance={24} />
+  //     </Canvas>
   //   </div>
   // );
+  return (
+    <nav className="space-x-4">
+      <Link to="/about" className="text-blue-600 hover:underline">
+        About
+      </Link>
+      <Link to="/tracks" className="text-blue-600 hover:underline">
+        Tracks
+      </Link>
+      <Link to="/faq" className="text-blue-600 hover:underline">
+        FAQ
+      </Link>
+      <Link to="/photos" className="text-blue-600 hover:underline">
+        Photos
+      </Link>
+    </nav>
+  );
 };
 
 export default Home;
