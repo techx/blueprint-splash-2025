7923474715fc75076ea633640a6bee9aec288268
lexue28 <lexue28@gmail.com>
2024-12-28 12:45:48 -0500

    started work on photo section

--- src/components/sections/Photos.tsx
+++ src/components/sections/Photos.tsx
@@ -1,7 +1,18 @@
+import Modal from "../Modal";
+import "./Photos.css";
+
 const Photos = () => {
   return (
     <div>
-      photos
+      <Modal className="w-[85%]">
+        <div className="overflow-x-auto">
+          <img
+            className="w-[300%] h-auto"
+            src="/images/Photos/photo string.svg"
+          />
+        </div>
+      </Modal>
+
     </div>
   );
 };
