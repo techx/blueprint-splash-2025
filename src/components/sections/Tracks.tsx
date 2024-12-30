29787cc1cbf00eaae8ebf611605bd6e6a66be721
gaurabd <gaurabd@mit.edu>
2024-12-29 23:20:42 -0600

    Add hover functionality

--- src/components/sections/Tracks.tsx
+++ src/components/sections/Tracks.tsx
@@ -25,7 +25,13 @@ const Tracks = () => {
           style={{ backgroundColor: "#F6EBCB" }}
           >
             {!isImageClicked && <img className="h-full w-auto" src="/src/assets/0_closed_box.png" alt="track" onClick={handleImageClick} />}
-            {isImageClicked && <img className="h-full w-auto" src="/src/assets/1_GIF.gif" alt="track" />}            
+            {isImageClicked && <div className="h-full w-auto">
+              <img className="h-full w-auto z-49" src="/src/assets/1_GIF.gif" alt="track" />
+              <div className="relative top-[-74%] left-[23%] h-[24%] w-[28%] z-50 opacity-0 hover:opacity-100 flex items-center justify-center text-wrap text-white" style={{ backgroundColor: 'transparent'}}>Beginner Web Dev</div>
+              <div className="relative top-[-98%] left-[52%] h-[24%] w-[28%] z-50 opacity-0 hover:opacity-100 flex items-center justify-center text-wrap text-white" style={{ backgroundColor: 'transparent'}}>Hardware</div>
+              <div className="relative top-[-96%] left-[21%] h-[24%] w-[28%] z-50 opacity-0 hover:opacity-100 flex items-center justify-center text-wrap text-white" style={{ backgroundColor: 'transparent'}}>Advanced Web Dev</div>
+              <div className="relative top-[-121%] left-[50%] h-[24%] w-[28%] z-50 opacity-0 hover:opacity-100 flex items-center justify-center text-wrap text-white" style={{ backgroundColor: 'transparent'}}> Game Development</div>
+            </div>}            
           </div>
         </div>
       </Modal>
