d0cf423fd8a6284e9b1358d82ab387f9f00dbfb1
Annie Wang <anniewang2023@gmail.com>
2025-01-03 21:44:32 -0500

    maybe fix sched mobile

--- src/components/mobile/ScheduleMobile.tsx
+++ src/components/mobile/ScheduleMobile.tsx
@@ -32,36 +32,35 @@ const ScheduleMobile = () => {
   // TODO: fix for ipad size, maybe distinguish md and smaller screen sizes
   return (
     <section id="Schedule">
-      <div className="relative h-[100vh]">
-        <img
-          src="/images/Schedule/sched_mobile.svg"
-          alt="Schedule Background"
-          className="w-full h-full min-h-[100vh] object-fill"
-          style={{ minHeight: "100%" }}
-        />
+      <div
+        className="relative h-[240vw]"
+        style={{
+          backgroundImage: "url('/images/Schedule/sched_mobile.svg')",
+          backgroundSize: "contain",
+          backgroundPosition: "top",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div className="absolute inset-0 text-pale-yellow h-[160vw] w-[60vw] mx-auto translate-y-[40vw] overflow-y-auto overflow-x-hidden">
+          <h2 className="text-4xl font-bold mb-3 break-words">
+            LEARNATHON [3/1]
+          </h2>
+          {Object.entries(learnathon_events).map(([time, event], index) => (
+            <div key={index} className="flex flex-row space-x-4">
+              <div className="w-3/4">{event}</div>
+              <div className="w-1/8 text-right whitespace-nowrap">{time}</div>
+            </div>
+          ))}
 
-        <div className="absolute inset-0 mx-9 px-10 my-10 pt-36">
-          <div className="text-pale-yellow max-h-[86vh] overflow-y-auto overflow-x-hidden">
-            <h2 className="text-4xl font-bold mb-3 break-words">
-              LEARNATHON [3/1]
-            </h2>
-            {Object.entries(learnathon_events).map(([time, event], index) => (
-              <div key={index} className="flex flex-row space-x-4">
-                <div className="w-3/4">{event}</div>
-                <div className="w-1/8 text-right whitespace-nowrap">{time}</div>
-              </div>
-            ))}
-
-            <h2 className="text-4xl font-bold mt-6 mb-3 break-words">
-              HACKATHON [3/2]
-            </h2>
-            {Object.entries(hackathon_events).map(([time, event], index) => (
-              <div key={index} className="flex flex-row space-x-4">
-                <div className="w-3/4">{event}</div>
-                <div className="w-1/8 text-right whitespace-nowrap">{time}</div>
-              </div>
-            ))}
-          </div>
+          <h2 className="text-4xl font-bold mt-6 mb-3 break-words">
+            HACKATHON [3/2]
+          </h2>
+          {Object.entries(hackathon_events).map(([time, event], index) => (
+            <div key={index} className="flex flex-row space-x-4">
+              <div className="w-3/4">{event}</div>
+              <div className="w-1/8 text-right whitespace-nowrap">{time}</div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
