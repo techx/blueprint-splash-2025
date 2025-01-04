f7151e2c0c2b55465acf92fe82072dab0092fca6
Annie Wang <anniewang2023@gmail.com>
2025-01-03 19:41:38 -0500

    fix photos mobile responsiveness

--- src/components/mobile/PhotosMobile.tsx
+++ src/components/mobile/PhotosMobile.tsx
@@ -56,15 +56,9 @@ const PhotosMobile = () => {
   const [openImageIndex, setOpenImageIndex] = useState<number | null>(null);
   const modalContainerRef = useRef<HTMLDivElement>(null);
 
-  const openModal = (index: number) => {
-    setOpenImageIndex(index);
-  };
+  const openModal = (index: number) => setOpenImageIndex(index);
+  const closeModal = () => setOpenImageIndex(null);
 
-  const closeModal = () => {
-    setOpenImageIndex(null);
-  };
-
-  // Close modal if clicked outside the modal container
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (
@@ -86,82 +80,74 @@ const PhotosMobile = () => {
 
   return (
     <section id="Photos">
-      <div className="flex flex-col items-center">
-        <div className="min-w-[90%] -translate-y-[5%]">
-          <div className="relative w-full h-[80vh]">
+      {/* Header with transparent background */}
+      <div
+        className="relative flex flex-col h-[66vh] bg-top bg-no-repeat bg-contain mt-20 mb-4"
+        style={{
+          backgroundImage: "url('/images/Photos/transparent_bg.svg')",
+        }}
+      >
+        <h1 className="relative text-5xl font-bold text-center mt-16 md:mt-28">
+          PHOTOS
+        </h1>
+
+        {/* Photo string and photos */}
+        <div className="relative mt-12 w-[100%] h-[34vh] overflow-hidden overflow-x-auto [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden">
+          {/* Photo string */}
+          <div className="relative w-[350vh] z-40">
             <img
-              src="/images/Photos/transparent_bg.svg"
-              className="absolute top-32 left-0 w-full object-cover"
+              src="/images/Photos/photo_string.svg"
+              alt="Photo String"
+              className="w-full z-40"
             />
           </div>
-          <h1 className="text-5xl font-bold absolute top-[30%] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            PHOTOS
-          </h1>
-        </div>
-        <div className="overflow-x-auto overflow-hidden w-[100%] h-[40vh] [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden absolute left-1/2 transform -translate-x-1/2 translate-y-[60%]">
-          <div className="relative">
-            <div className="w-[340vh] mx-auto absolute z-40">
-              <img
-                src="/images/Photos/photo_string.svg"
-                alt="Photo String"
-                className="w-full z-40"
-              />
-            </div>
 
-            <div className="flex flex-row flex-start gap-9 absolute mx-auto translate-x-2 top-3 w-[340vh] z-10">
-              {imageData.map((item, index) => (
-                <div
-                  key={index}
-                  className="relative cursor-pointer transition-transform transform hover:scale-110 w-[250px] h-[250px] z-10"
-                >
-                  <img
-                    className="absolute inset-0 w-full h-full z-10"
-                    src="/images/Photos/polaroid_bg.svg"
-                    alt="Polaroid background"
-                  />
-                  <div className="absolute top-[16%] left-[8%] w-[84%] h-[60%] overflow-hidden z-20">
-                    <img
-                      className="w-full h-full object-cover z-20"
-                      src={`/src/assets/${item.src}`}
-                      alt={`Thumbnail ${index + 1}`}
-                      onClick={() => openModal(index)}
-                    />
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-
-        {/* Large image pop up */}
-        {openImageIndex !== null && (
-          <div className="fixed inset-0 z-50">
-            <div
-              className="absolute inset-0 bg-black/70"
-              onClick={closeModal}
-            ></div>
-            <div className="fixed inset-0 flex items-center justify-center">
+          {/* Photos */}
+          <div className="relative flex flex-row flex-start gap-[3.8vh] w-[350vh] translate-x-2 -translate-y-10 md:-translate-y-12 z-10">
+            {imageData.map((item, index) => (
               <div
-                ref={modalContainerRef}
-                className="bg-polaroid h-[50vh] w-[90vw] rounded-xl p-8 shadow-2xl overflow-hidden flex flex-col z-60 relative"
+                key={index}
+                className="transition-transform transform w-[31vh] h-[31vh] z-10"
+                onClick={() => openModal(index)}
               >
-                <div className="flex flex-col h-full">
-                  <div className="flex-1 overflow-hidden">
-                    <img
-                      src={`/src/assets/${imageData[openImageIndex].src}`}
-                      alt={`Large View ${openImageIndex + 1}`}
-                      className="w-full h-full object-contain"
-                    />
-                  </div>
-                  <p className="mt-6 text-center text-lg min-h-[3rem]">
-                    {imageData[openImageIndex].caption}
-                  </p>
+                <img
+                  src="/images/Photos/polaroid_bg.svg"
+                  alt="Polaroid background"
+                  className="w-full h-full"
+                />
+                <div className="absolute top-[16%] left-[7%] w-[85%] h-[55%]">
+                  <img
+                    src={`/src/assets/${item.src}`}
+                    alt={`Thumbnail ${index + 1}`}
+                    className="w-full h-full object-cover"
+                  />
                 </div>
               </div>
-            </div>
+            ))}
           </div>
-        )}
+        </div>
       </div>
+
+      {/* Modal */}
+      {openImageIndex !== null && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/70">
+          <div
+            ref={modalContainerRef}
+            className="bg-polaroid h-[60vh] w-[90vw] rounded-xl p-8 shadow-2xl flex flex-col"
+          >
+            <div className="flex-1 overflow-hidden">
+              <img
+                src={`/src/assets/${imageData[openImageIndex].src}`}
+                alt={`Large View ${openImageIndex + 1}`}
+                className="w-full h-full object-contain"
+              />
+            </div>
+            <p className="mt-4 text-center text-lg">
+              {imageData[openImageIndex].caption}
+            </p>
+          </div>
+        </div>
+      )}
     </section>
   );
 };
