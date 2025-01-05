3bb4f836816d881be768c280a98757bb67170155
Annie Wang <anniewang2023@gmail.com>
2025-01-05 02:23:15 -0500

    delete imports

--- src/components/sections/Home.tsx
+++ src/components/sections/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Canvas, useThree } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
