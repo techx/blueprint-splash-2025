82980946788052d4d866be4857e99f77a6fc1c69
Sarah <sarahsu@mit.edu>
2025-01-05 17:20:40 -0800

    fixed faq section

--- src/components/sections/FAQ.tsx
+++ src/components/sections/FAQ.tsx
@@ -16,88 +16,188 @@ const Faq = () => {
   const faqItems = [
     {
       title: "What is a learnathon?",
-      content:
-        "A day of learning skills in computer science! You will have the opportunity to attend a day of workshops in your chosen track: web dev (beginner, advanced), game dev (beginner, advanced), or hardware. See the tracks section above for more info. Beyond track-specific workshops, we'll also have auxiliary workshops, tech talks, an organization fair, and more.",
+      content: (
+        <>
+          A day of learning skills in computer science! You will have the
+          opportunity to attend a day of workshops in your chosen track: web dev
+          (beginner, advanced), game dev (beginner, advanced), or hardware. See
+          the tracks section above for more info. Beyond track-specific
+          workshops, we'll also have auxiliary workshops, tech talks, an
+          organization fair, and more.
+        </>
+      ),
     },
     {
       title: "What is a hackathon?",
-      content:
-        "A chance to build an amazing computer science project! Students who attend the learnathon will apply their newly-acquired skills to create something they can call their own. Websites and mobile apps are common hacks, but you can build anything. We encourage you to think outside the box!",
+      content: (
+        <>
+          A chance to build an amazing computer science project! Students who
+          attend the learnathon will apply their newly-acquired skills to create
+          something they can call their own. Websites and mobile apps are common
+          hacks, but you can build anything. We encourage you to think outside
+          the box!
+        </>
+      ),
     },
     {
       title: "How much experience do I need to attend?",
-      content:
-        "Zero computer science, coding, or hacking experience is needed! We encourage students of all skill levels to apply—many of our workshops assume you have no experience and will help build your coding abilities from the ground up.",
+      content: (
+        <>
+          Zero computer science, coding, or hacking experience is needed! We
+          encourage students of all skill levels to apply—many of our workshops
+          assume you have no experience and will help build your coding
+          abilities from the ground up.
+        </>
+      ),
     },
     {
       title: "Do I need to attend both the learnathon and hackathon?",
-      content:
-        "No, you can attend either one or both days! However, only participants who participate in track workshops during the learnathon or submit a project for the hackathon will be eligible for swag.",
+      content: (
+        <>
+          No, you can attend either one or both days! However, only participants
+          who participate in track workshops during the learnathon or submit a
+          project for the hackathon will be eligible for swag.
+        </>
+      ),
     },
     {
       title: "How will teams work? What if I don't have a team?",
-      content:
-        "During our hackathon, you'll have the opportunity to create a project in a team of up to 4 students. Don't worry if you don't have a team now—we'll have team formation and ideation sessions, so there'll be plenty of time to meet new people!",
+      content: (
+        <>
+          During our hackathon, you'll have the opportunity to create a project
+          in a team of up to 4 students. Don't worry if you don't have a team
+          now—we'll have team formation and ideation sessions, so there'll be
+          plenty of time to meet new people!
+        </>
+      ),
     },
     {
       title: "Why is there an application?",
-      content:
-        "While we want to open Blueprint to everyone, having an application allows us to provide a better event experience with respect to planning and logistics. However, you do not need any experience to apply. We're looking for people who are passionate about learning and creating!",
+      content: (
+        <>
+          While we want to open Blueprint to everyone, having an application
+          allows us to provide a better event experience with respect to
+          planning and logistics. However, you do not need any experience to
+          apply. We're looking for people who are passionate about learning and
+          creating!
+        </>
+      ),
     },
     {
       title: "I have more questions!",
-      content:
-        "Feel free to reach out to us at blueprint@hackmit.org for any questions, concerns, or just to say hi :)",
+      content: (
+        <>
+          Feel free to reach out to us at <strong>blueprint@hackmit.org</strong>{" "}
+          for any questions, concerns, or just to say hi {":)"}
+        </>
+      ),
     },
     {
       title: "When is Blueprint?",
-      content:
-        "Blueprint will be from March 1 to March 2. There will be a learnathon on Saturday March 1, and a hackathon on Sunday March 2.",
+      content: (
+        <>
+          Blueprint will be from <strong>March 1 to March 2</strong>. There will
+          be a learnathon on Saturday March 1, and a hackathon on Sunday March
+          2.
+        </>
+      ),
     },
     {
       title: "Where is Blueprint?",
-      content:
-        "The Saturday learnathon will take place at MIT's Stata Center (32 Vassar St, Cambridge, MA 02139). The Sunday hackathon will take place at the MIT Media Lab (75 Amherst St, Cambridge, MA 02139).",
+      content: (
+        <>
+          The Saturday learnathon will take place at MIT's Stata Center (32
+          Vassar St, Cambridge, MA 02139). The Sunday hackathon will take place
+          at the MIT Media Lab (75 Amherst St, Cambridge, MA 02139).
+        </>
+      ),
     },
     {
       title: "Will Blueprint be fully in-person?",
-      content:
-        "Yes – the events over the weekend will have no virtual component. However, we may potentially host virtual events during Blueprint week, which will be open to everyone.",
+      content: (
+        <>
+          Yes – the events over the weekend will have no virtual component.
+          However, we may potentially host virtual events during Blueprint week,
+          which will be open to everyone.
+        </>
+      ),
     },
     {
       title: "Am I eligible to attend?",
-      content:
-        "If you're a high school student, we invite you to apply - no coding experience required! Blueprint will teach you all you need to know to compete in your first hackathon, so beginners are more than welcome!",
+      content: (
+        <>
+          If you're a high school student, we invite you to apply - no coding
+          experience required! Blueprint will teach you all you need to know to
+          compete in your first hackathon, so beginners are more than welcome!
+        </>
+      ),
     },
     {
       title: "Does Blueprint cost money?",
-      content:
-        "Admission is free and includes mentors, workshops, swag, resources, and an unforgettable experience! ",
+      content: (
+        <>
+          Admission is free and includes mentors, workshops, swag, resources,
+          and an unforgettable experience!
+        </>
+      ),
     },
     {
       title: "What will I eat?",
-      content:
-        "We will provide breakfast and lunch for Saturday's learnathon, and breakfast, lunch, and dinner for Sunday's hackathon for free! We also have options for those with dietary restrictions. There will also be a snack bar on Sunday :)",
+      content: (
+        <>
+          We will provide breakfast and lunch for Saturday's learnathon, and
+          breakfast, lunch, and dinner for Sunday's hackathon for free! We also
+          have options for those with dietary restrictions. There will also be a
+          snack bar on Sunday {":)"}
+        </>
+      ),
     },
     {
       title: "When is the application deadline?",
-      content:
-        "Applications are due February 3rd, 11:59pm Eastern Time (please note the timezone!). No extensions will be granted unless there are special circumstances; in that case, please email blueprint@hackmit.org.",
+      content: (
+        <>
+          Applications are due{" "}
+          <strong>February 3rd, 11:59pm Eastern Time</strong> (please note the
+          timezone!). No extensions will be granted unless there are special
+          circumstances; in that case, please email blueprint@hackmit.org.
+        </>
+      ),
     },
     {
       title: "Can I volunteer to judge or mentor?",
-      content:
-        "If you're in college or beyond, we will consider your application! We will update this link when our application for judging/mentoring opens. ",
+      content: (
+        <>
+          If you're in college or beyond, we will consider your application! We
+          will update this link when our application for judging/mentoring
+          opens.
+        </>
+      ),
     },
     {
       title: "Are you doing travel reimbursements?",
-      content:
-        "Due to logistical & financial restrictions, we cannot reimburse lodging expenses for this event, and in order to provide our attendees with the best possible experience, we will not be able to accommodate virtual hackers. Travel subsidies may be available through the support of Hack Club's Gas Fund, but these details are still tentative and we cannot guarantee travel support.",
+      content: (
+        <>
+          Due to logistical & financial restrictions, we cannot reimburse
+          lodging expenses for this event, and in order to provide our attendees
+          with the best possible experience, we will not be able to accommodate
+          virtual hackers. Travel subsidies may be available through the support
+          of Hack Club's Gas Fund, but these details are still tentative and we
+          cannot guarantee travel support.
+        </>
+      ),
     },
     {
       title: "Will you be hosting students?",
-      content:
-        "Unfortunately, MIT administration has strict requirements regarding hosting minors on campus, so we would not be able to match you with a host through the Blueprint team. If you aren't able to commute to Blueprint and happen to know an MIT student or a student in the Boston area, you could try reaching out to them personally to see if they are able to host you!",
+      content: (
+        <>
+          Unfortunately, MIT administration has strict requirements regarding
+          hosting minors on campus, so we would not be able to match you with a
+          host through the Blueprint team. If you aren't able to commute to
+          Blueprint and happen to know an MIT student or a student in the Boston
+          area, you could try reaching out to them personally to see if they are
+          able to host you!
+        </>
+      ),
     },
   ];
   const generalQuestions = faqItems.slice(0, 7);
@@ -149,7 +249,7 @@ const Faq = () => {
                   <div className="w-full">
                     <div
                       onClick={() => toggleItem(index, "general")}
-                      className="cursor-pointer font-bold mb-[5px] text-xl text-dark-green"
+                      className="cursor-pointer font-bold mb-[5px] text-xl text-dark-green hover:text-light-green"
                     >
                       {item.title}
                     </div>
@@ -198,7 +298,7 @@ const Faq = () => {
                   <div className="w-full">
                     <div
                       onClick={() => toggleItem(index, "logistics")}
-                      className="cursor-pointer font-bold mb-[5px] text-[20px] text-dark-green"
+                      className="cursor-pointer font-bold mb-[5px] text-[20px] text-dark-green hover:text-light-green"
                     >
                       {item.title}
                     </div>
